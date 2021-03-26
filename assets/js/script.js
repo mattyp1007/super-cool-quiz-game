1d08ec92b47be727082e048bc0f37e66b8804cb7
Matthew Parker <matt.parker1007@gmail.com>
2021-03-26 18:06:37 -0500

    completed renderQuestion function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,29 @@
 // Question set 
-
-// The correct answer is Option A, or a[0]
-var q1 = {
-    q: "This is question 1.",
-    a: ["Option A", "Option B", "Option C", "Option D"],
-    correct: a[0]
-}
-
-// The correct answer is Option D, or a[3]
-var q2 = {
-    q: "This is question 2.",
-    a: ["Option A", "Option B", "Option C", "Option D"],
-    correct: a[3]
-}
+var questions = [
+    {
+        q: "This is question 1.",
+        a: ["Option A", "Option B", "Option C", "Option D"],
+        correct: 0
+    },
+    {
+        q: "This is question 2.",
+        a: ["Option A", "Option B", "Option C", "Option D"],
+        correct: 3
+    }
+];
+var questionNumEl = document.getElementById("questionNum");
+var questionContentEl = document.getElementById("questionContent");
+var choicesEls = document.getElementsByClassName("choice");
 
 function renderQuestion(qNum){
-    
+    // display question number
+    questionNumEl.textContent = qNum;
+    // display question content
+    questionContentEl.textContent = questions[qNum-1].q;
+    // sets text content for answer choices
+    for(var i=0; i < 4; i++){
+        choicesEls[i].textContent = questions[qNum-1].a[i];
+    }
 }
+
+renderQuestion(1);
