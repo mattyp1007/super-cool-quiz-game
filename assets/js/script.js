9f428f8c0c051d846250b212bdcb1f5e253f5820
Matthew Parker <matt.parker1007@gmail.com>
2021-03-26 17:06:41 -0500

    made some question objects for future testing and assuring my logic

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Question set 
+
+// The correct answer is Option A, or a[0]
+var q1 = {
+    q: "This is question 1.",
+    a: ["Option A", "Option B", "Option C", "Option D"],
+    correct: a[0]
+}
+
+// The correct answer is Option D, or a[3]
+var q2 = {
+    q: "This is question 2.",
+    a: ["Option A", "Option B", "Option C", "Option D"],
+    correct: a[3]
+}
+
+function renderQuestion(qNum){
+    
+}
